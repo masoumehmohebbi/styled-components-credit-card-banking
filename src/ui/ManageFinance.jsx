69a4f413b2df2f8bd1139901f0cfcc4d3ad079bd
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-02-10 15:29:51 +0330

    ✅ Updated Images Src

--- src/ui/ManageFinance.jsx
+++ src/ui/ManageFinance.jsx
@@ -2,6 +2,8 @@ import { Wrapper, WrapperProcces } from "../styles/globalStyles";
 import { Heading } from "./Heading";
 import { SubHeading } from "./SubHeading";
 import EasyManageFinance from "/images/ManageFinance.svg";
+import AppStore from "/images/App_Store.svg";
+import GooglePlay from "/images/Google_Play.svg";
 
 const ManageFinance = () => {
   return (
@@ -14,8 +16,8 @@ const ManageFinance = () => {
         </SubHeading>
 
         <Wrapper>
-          <img className="app-icon" src="/images/App_Store.svg" alt="" />
-          <img className="app-icon" src="/images/Google_Play.svg" alt="" />
+          <img className="app-icon" src={AppStore} alt="app store" />
+          <img className="app-icon" src={GooglePlay} alt="google play" />
         </Wrapper>
       </div>
     </WrapperProcces>
